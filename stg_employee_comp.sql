f1a3933c47dcf9db52115af8fa118904646b161a
zkrat <zachariah.kratz@gmail.com>
2025-11-26 10:56:33 -0600

    changed some constraints and created the database name

--- stg_employee_comp.sql
+++ stg_employee_comp.sql
@@ -1,14 +1,14 @@
-    
+CREATE DATABASE chicago_employees
     
 CREATE TABLE stg_employee_comp (
     emp_id           SERIAL PRIMARY KEY,
-    full_name        VARCHAR(50),
-    department       VARCHAR(50),
-    job_title        VARCHAR(50),
-    full_or_part     VARCHAR(25),       -- 'F'/'P' or 'FULL TIME'/'PART TIME'
+    full_name        VARCHAR(100),
+    department       VARCHAR(100),
+    job_title        VARCHAR(100),
+    full_or_part     VARCHAR(50),       -- 'F'/'P' or 'FULL TIME'/'PART TIME'
     salary_or_hourly CHAR(6),
-    salary           NUMERIC(12,2) GENERATED ALWAYS AS (hourly_rate * weekly_hours) STORED -- annual salary (nullable)
-    hourly_rate      NUMERIC(10,2) GENERATED ALWAYS AS (salary / 40) STORED, -- hourly rate (nullable)
+    salary           NUMERIC(12,2) DEFAULT 0, -- annual salary (nullable)
+    hourly_rate      NUMERIC(10,2) DEFAULT 0, -- hourly rate (nullable)
     --generated columns cannot be written to directly. it 
     weekly_hours     INTEGER DEFAULT 40,       -- 10, 20, 35, 40 (nullable)
     est_annual_pay   NUMERIC(12,2), -- feature-engineered (optional)
