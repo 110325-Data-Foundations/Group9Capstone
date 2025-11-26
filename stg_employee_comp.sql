e1d90f7c8c6d3eb90a60cd5b6567592ce7ae92ca
zkrat <zachariah.kratz@gmail.com>
2025-11-26 10:09:56 -0600

    created the database. neep python code to connect to python

--- stg_employee_comp.sql
+++ stg_employee_comp.sql
@@ -2,22 +2,23 @@
     
 CREATE TABLE stg_employee_comp (
     emp_id           SERIAL PRIMARY KEY,
-    full_name        TEXT,
-    department       TEXT,
-    job_title        TEXT,
-    full_or_part     TEXT,          -- 'F'/'P' or 'FULL TIME'/'PART TIME'
-    salary           NUMERIC(12,2), -- annual salary (nullable)
-    hourly_rate      NUMERIC(10,2), -- hourly rate (nullable)
-    frequency        INTEGER,       -- 10, 20, 35, 40 (nullable)
+    full_name        VARCHAR(50),
+    department       VARCHAR(50),
+    job_title        VARCHAR(50),
+    full_or_part     VARCHAR(25),       -- 'F'/'P' or 'FULL TIME'/'PART TIME'
+    salary_or_hourly CHAR(6),
+    salary           NUMERIC(12,2) GENERATED ALWAYS AS (hourly_rate * weekly_hours) STORED -- annual salary (nullable)
+    hourly_rate      NUMERIC(10,2) GENERATED ALWAYS AS (salary / 40) STORED, -- hourly rate (nullable)
+    --generated columns cannot be written to directly. it 
+    weekly_hours     INTEGER DEFAULT 40,       -- 10, 20, 35, 40 (nullable)
     est_annual_pay   NUMERIC(12,2), -- feature-engineered (optional)
-    is_manager       BOOLEAN,       -- feature-engineered (optional)
     load_ts          TIMESTAMPTZ DEFAULT NOW(),
-    source_file      TEXT
+    department_avg   NUMERIC(12,2)  -- feature-engineered (optional)
 );
 
 CREATE TABLE stg_rejects (
     rejected_id SERIAL PRIMARY KEY,
-    raw_record JSONB,
+    raw_record JSON,
     error_reason TEXT,
     load_ts TIMESTAMPTZ DEFAULT NOW(),
     source_file TEXT
