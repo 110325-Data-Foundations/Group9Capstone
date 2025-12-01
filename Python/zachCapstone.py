032189e0a36a29b099c4942dbf6a7545f3a56f1d
zkrat <zachariah.kratz@gmail.com>
2025-12-01 14:24:27 -0600

    made minimal import changes but forgot to change to my branch

--- Python/zachCapstone.py
+++ Python/zachCapstone.py
@@ -1,8 +1,12 @@
 import pandas as pd
-from sqlalchemy import create_engine #creates our database engine
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import Python.zachCalculations as zach
+from sqlalchemy import create_engine #this creates our database engine
 from dotenv import load_dotenv #lets us read from our .env file
 import os
-import json
-import Python.zachCalculations as zach
+from adjustText import adjust_text
 
 
