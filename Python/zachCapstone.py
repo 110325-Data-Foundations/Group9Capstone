0ab8bc060be098240ce31c9bb1e8e8d9459e725c
zkrat <zachariah.kratz@gmail.com>
2025-12-02 15:00:17 -0600

    completed pytesting and added my pictures for the presentation

--- Python/zachCapstone.py
+++ Python/zachCapstone.py
@@ -39,6 +39,31 @@ departments_with_part_time = df.loc[df['full_or_part'] == 'P', 'department'].uni
 # Filter avgHourly_df to only those departments
 avgHourly_df = avgHourly_df[avgHourly_df['department'].isin(departments_with_part_time)]
 
+
+# This calculates the average overall of the part time, full time, and salaried hourly rates
+avgOverall = filtered.groupby(['salary_or_hourly','full_or_part'])['hourly_rate'].mean().reset_index()
+
+
+#The below code goes through the df and cleans up the appearance of the columns and column names
+def label_row(row):
+    if row['salary_or_hourly'] == 'HOURLY' and row['full_or_part'] == 'F':
+        return 'Full-Time Hourly'
+    elif row['salary_or_hourly'] == 'HOURLY' and row['full_or_part'] == 'P':
+        return 'Part-Time Hourly'
+    elif row['salary_or_hourly'] == 'SALARY':
+        return 'Salaried'
+    else:
+        return 'Other'
+
+avgOverall['category'] = avgOverall.apply(label_row, axis=1)
+
+# Step 4: Display results
+print(avgOverall[['category','hourly_rate']])
+
+
+
+
+
 # Pivot so each category becomes a column
 pivot_df = avgHourly_df.pivot_table(
     index='department',
@@ -60,3 +85,4 @@ for label in plt.gca().get_xticklabels():
 plt.legend(title='Category', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.show()
+
--- Tests/test_zachCalculations.py
+++ Tests/test_zachCalculations.py
@@ -2,6 +2,7 @@ import sys
 import os
 import Python.zachCalculations as zach
 
+
 # Make Python see the project root
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -10,7 +11,7 @@ def test_calculate_salary():
     hourly_rate =10
     weekly_hours=40
     wages=hourly_rate*weekly_hours*52
-    assert zach.calculate_salary(10,40)==round(float(wages,2))
+    assert zach.calculate_salary(10,40)==wages
 
 def test_calculate_hourly():
     salary=50000
